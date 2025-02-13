92e1e63d367fe71c159c91d8d354b5d1409f7b94
jordip0630 <jorpaigom@alu.edu.gva.es>
2025-02-13 14:49:59 +0100

    Pequeños avances en los Modelos y Controllers

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,13 +10,27 @@ use App\Models\Admin;
 
 class AdminController extends Controller
 {
-    public function mostrarOpticas(){
+    /*public function mostrarOpticas(){
 
         $admins=Admin::with('opticas')->get();
 
         foreach ($admins as $key => $admin) {
             echo $admin->opticas;
         }
+    }*/
+
+    public function buscarAdmin($idAdmin){
+
+        $admin=Admin::where('id', $idAdmin)->get();
+
+        return response()->json($admin);
+    }
+
+    public function index(Request $request)
+    {
+        $admin = Admin::all();
+        return response()->json($admin);
+        //Devuelve un listado con todos los admins
     }
 
     public function mostrarAdmins(Request $request)
@@ -31,7 +45,7 @@ class AdminController extends Controller
         $request->validate([
             'nombre' => 'required|string|max:20',
             'apellido' => 'required|string|max:20',
-            'dni' => 'required|string|min:9',
+            'dni' => 'required|string|min:9', 
             'direccion'=> 'required|string|max:40',
             'telefono'=> 'required|integer|max:15',
             'correo'=> 'required|email',
