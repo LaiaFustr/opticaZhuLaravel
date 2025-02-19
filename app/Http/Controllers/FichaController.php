72557176f95375a42b237aefcc2a1c09cae201e3
Laia Fuster <laiafuster97@gmail.com>
2025-02-19 08:55:16 +0100

    en que momento

--- app/Http/Controllers/FichaController.php
+++ app/Http/Controllers/FichaController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 
 class FichaController extends Controller
 {
-   /*  public function ficha($id){
+    public function creaFicha($id){
         $cita = Cita::find($id);
-
-    } */
+        $datosFicha = request()->validate([''])
+    }
 }
