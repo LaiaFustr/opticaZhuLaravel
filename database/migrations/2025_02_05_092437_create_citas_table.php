ba4bc3f43652d041cd5a3994bbae66db74d01a0c
jordip0630 <jorpaigom@alu.edu.gva.es>
2025-02-21 09:34:25 +0100

    me quedo quieto

--- database/migrations/2025_02_05_092437_create_citas_table.php
+++ database/migrations/2025_02_05_092437_create_citas_table.php
@@ -23,9 +23,7 @@ return new class extends Migration
 
             $table->foreign('idOptometrista')->references('id')->on('optometristas')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('idCliente')->references('id')->on('clientes')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('idOptica')->references('id')->on('opticas')->onUpdate('cascade')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('idBloqueHorario')->references('id')->on('bloque_horario')->onUpdate('cascade')->onDelete('cascade')->onUpdate('cascade');
-        });
+            $table->foreign('idOptica')->references('id')->on('opticas')->onUpdate('cascade')->onDelete('cascade')->onUpdate('cascade');        });
     }
 
     /**
