827cb226019e7f416c27b16e884392871d59041c
Laia Fuster <laiafuster97@gmail.com>
2025-02-24 08:42:40 +0100

    micommit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,8 +12,9 @@
     <link href="/coreui/coreui.min.css" rel="stylesheet" >
     <!-- <script src="/coreui/bundle.min.css"></script> -->
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="/bootstrap/bootstrap.min.css"/>
-    <script defer src="/bootstrap/bootstrap.bundle.min.js"></script>
+   <!--  <link rel="stylesheet" href="/bootstrap/bootstrap.min.css"/>
+    <script defer src="/bootstrap/bootstrap.bundle.min.js"></script> -->
+    @vite('resources/js/app.js');
     <!-- font awesome -->
     <link href="/font-awesome/all.min.css" rel="stylesheet">
     <!-- jQuery -->
